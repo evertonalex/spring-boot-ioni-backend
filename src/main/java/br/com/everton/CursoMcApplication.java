a9dd1542a7f478e9440d40f9b377c39a371b6764
everton_alex <evertonalexweb@gmail.com>
2018-05-12 14:50:55 -0300

    primeiro versao curso udemy spring boot ionic

---
+++ src/main/java/br/com/everton/CursoMcApplication.java
@@ -0,0 +1,12 @@
+package br.com.everton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoMcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoMcApplication.class, args);
+	}
+}
