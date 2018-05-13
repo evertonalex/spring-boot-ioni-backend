0624902215dd401f81a53ff5ce0485e9427733d1
evertonalex <evertonalexweb@gmail.com>
2018-05-13 10:44:35 -0300

    cidade e estado

--- src/main/java/br/com/everton/CursoMcApplication.java
+++ src/main/java/br/com/everton/CursoMcApplication.java
@@ -10,8 +10,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.mysql.fabric.xmlrpc.base.Array;
 
 import br.com.everton.domain.Categoria;
+import br.com.everton.domain.Cidade;
+import br.com.everton.domain.Estado;
 import br.com.everton.domain.Produto;
 import br.com.everton.repositories.CategoriaRepository;
+import br.com.everton.repositories.CidadeRepository;
+import br.com.everton.repositories.EstadoRepository;
 import br.com.everton.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -22,6 +26,12 @@ public class CursoMcApplication implements CommandLineRunner{
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoMcApplication.class, args);
@@ -45,6 +55,19 @@ public class CursoMcApplication implements CommandLineRunner{
 		
 		//categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		//produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "minas gerais");
+		Estado est2 = new Estado(null, "sao paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlandia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 		
 	}
 }
---
+++ src/main/java/br/com/everton/repositories/CidadeRepository.java
@@ -0,0 +1,12 @@
+package br.com.everton.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.everton.domain.Categoria;
+import br.com.everton.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+	
+}
