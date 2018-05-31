f5edf010688a738641af228142171cca49521557
evertonalex <evertonalexweb@gmail.com>
2018-05-31 13:55:06 -0300

    email nao repetido na ataulizacao do cliente

--- src/main/java/br/com/everton/dto/ClienteDTO.java
+++ src/main/java/br/com/everton/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import br.com.everton.domain.Cliente;
+import br.com.everton.services.validator.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
---
+++ src/main/java/br/com/everton/services/validator/ClienteUpdateValidator.java
@@ -0,0 +1,57 @@
+package br.com.everton.services.validator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import br.com.everton.domain.Cliente;
+import br.com.everton.domain.enums.TipoCliente;
+import br.com.everton.dto.ClienteDTO;
+import br.com.everton.repositories.ClienteRepository;
+import br.com.everton.resources.exception.FieldMessage;
+import br.com.everton.services.validator.utils.BR;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+	
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+		
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));
+		
+		List<FieldMessage> list = new ArrayList<>();
+				
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		
+		System.out.println("AUX" + aux);
+		
+		if(aux != null && !aux.getId().equals(uriId)) {
+			System.out.println("EMAIL JA EXISTENTE");
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+		
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFiledName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
