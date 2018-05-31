e8c0117f8df2263e412fbdc1ca85c7aaab599eda
evertonalex <evertonalexweb@gmail.com>
2018-05-31 10:59:53 -0300

    inserindo cliente com POST

--- src/main/java/br/com/everton/resources/ClienteResource.java
+++ src/main/java/br/com/everton/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package br.com.everton.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.everton.domain.Cliente;
 import br.com.everton.dto.ClienteDTO;
+import br.com.everton.dto.ClienteNewDTO;
 import br.com.everton.services.ClienteService;
 
 @RestController
@@ -73,4 +76,15 @@ public class ClienteResource {
 		return ResponseEntity.ok().body(listDto);
 	}
 
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto){
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		System.out.println("TESTE DE SALVAR CATEGORIA  NOME --- > " + obj.getNome());
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
+	}
+	
 }
--- src/main/java/br/com/everton/services/ClienteService.java
+++ src/main/java/br/com/everton/services/ClienteService.java
@@ -3,6 +3,8 @@ package br.com.everton.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
@@ -10,9 +12,15 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import br.com.everton.domain.Cidade;
 import br.com.everton.domain.Cliente;
+import br.com.everton.domain.Endereco;
+import br.com.everton.domain.enums.TipoCliente;
 import br.com.everton.dto.ClienteDTO;
+import br.com.everton.dto.ClienteNewDTO;
+import br.com.everton.repositories.CidadeRepository;
 import br.com.everton.repositories.ClienteRepository;
+import br.com.everton.repositories.EnderecoRepository;
 import br.com.everton.services.exceptions.DataIntegrityException;
 import br.com.everton.services.exceptions.ObjectNotFoundException;
 
@@ -21,13 +29,28 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repo;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto nao encontrado! id:" + id + ", Tipo: " + Cliente.class.getName()));
 	}
+
 	
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		enderecoRepository.saveAll(obj.getEnderecos());
+		
+		return obj;
+	}
 
 	public Cliente update(Cliente obj) {
 		Cliente newObj = find(obj.getId());
@@ -56,6 +79,24 @@ public class ClienteService {
 	
 	public Cliente fromDTO(ClienteDTO objDto) {
 		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDTO) {
+		Cliente cli = new Cliente(null, objDTO.getNome(), objDTO.getEmail(), objDTO.getCpfOuCnpj(), TipoCliente.toEnum(objDTO.getTipo()));
+		Cidade cid = cidadeRepository.getOne(objDTO.getCidadeID());
+		
+		Endereco end = new Endereco(null, objDTO.getLogradouro(), objDTO.getNumero(), objDTO.getComplemento(), objDTO.getBairro(), objDTO.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		
+		cli.getTelefones().add(objDTO.getTelefone1());
+		if(objDTO.getTelefone2() != null) {
+			cli.getTelefones().add(objDTO.getTelefone2());
+		}
+		if(objDTO.getTelefone3() != null) {
+			cli.getTelefones().add(objDTO.getTelefone3());
+		}
+		
+		return cli;
 	}
 
 	private void updateData(Cliente newObj, Cliente obj) {
