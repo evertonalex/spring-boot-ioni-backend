6085c327b1546bc12284fd35805e9b298cb2c3fc
EvertonAlex <evertonalexweb@gmail.com>
2018-06-10 11:15:22 -0300

    to string de pedido MOEDA E DATA

--- src/main/java/br/com/everton/domain/Pedido.java
+++ src/main/java/br/com/everton/domain/Pedido.java
@@ -1,8 +1,11 @@
 package br.com.everton.domain;
 
 import java.io.Serializable;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.Locale;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
@@ -16,6 +19,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.sun.org.apache.bcel.internal.generic.SIPUSH;
 
 @Entity
 public class Pedido implements Serializable{
@@ -133,6 +137,31 @@ public class Pedido implements Serializable{
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	@Override
+	public String toString() {
+
+		NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy hh:mm:ss");
+
+		final StringBuilder builder = new StringBuilder();
+		builder.append("Pedido número");
+		builder.append(getId());
+		builder.append(", Instante: ");
+		builder.append(sdf.format(getInstante()));
+		builder.append(", Cliente: ");
+		builder.append(getCliente().getNome());
+		builder.append(", Situacao do pagamento: ");
+		builder.append(getPagamento().getEstado().getDescricao());
+		builder.append("\n Detalhas \n");
+		for(ItemPedido ip : getItens()){
+			builder.append(ip.toString());
+		}
+		builder.append("Valor Total");
+		builder.append(nf.format(getValorTotal()));
+
+		return builder.toString();
 	}
 	
 }
