3afe4eed8272dc4ef9402102f2b5ac264359d3fc
EvertonAlex <evertonalexweb@gmail.com>
2018-06-16 15:13:44 -0300

    autenticacao de usuario JWT

---
+++ src/main/java/br/com/everton/services/UserDetailServiceImpl.java
@@ -0,0 +1,27 @@
+package br.com.everton.services;
+
+import br.com.everton.domain.Cliente;
+import br.com.everton.repositories.ClienteRepository;
+import br.com.everton.security.UserSS;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService{
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Cliente cli = clienteRepository.findByEmail(email);
+
+        if(cli == null){
+            throw new UsernameNotFoundException(email);
+        }
+        return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+    }
+}
