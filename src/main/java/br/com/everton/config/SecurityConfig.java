9aa664a54824cb9221a378b1121a08c66ae45a3a
EvertonAlex <evertonalexweb@gmail.com>
2018-06-16 16:07:39 -0300

    autorizacao JWT

--- src/main/java/br/com/everton/config/SecurityConfig.java
+++ src/main/java/br/com/everton/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package br.com.everton.config;
 
 import br.com.everton.security.JWTAuthenticationFilter;
+import br.com.everton.security.JWTAuthorizationFilter;
 import br.com.everton.security.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -54,6 +55,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(PUBLIC_MATCHERS).permitAll()
                 .anyRequest().authenticated();
         http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+        http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
