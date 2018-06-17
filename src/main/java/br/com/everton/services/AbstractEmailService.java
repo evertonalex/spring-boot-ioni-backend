e14f865f4565a816d8335a2a98b33b9b9bc2e16a
EvertonAlex <evertonalexweb@gmail.com>
2018-06-17 10:02:37 -0300

    esqueci a senha

--- src/main/java/br/com/everton/services/AbstractEmailService.java
+++ src/main/java/br/com/everton/services/AbstractEmailService.java
@@ -1,5 +1,6 @@
 package br.com.everton.services;
 
+import br.com.everton.domain.Cliente;
 import br.com.everton.domain.Pedido;
 import java.util.Date;
 
@@ -24,6 +25,22 @@ public abstract class AbstractEmailService implements EmailService {
         sm.setSubject("Pedido confirmado! Código: " + obj.getId());
         sm.setSentDate(new Date(System.currentTimeMillis()));
         sm.setText(obj.toString());
+        return sm;
+    }
+
+    @Override
+    public void sendNewpasswordEmail(Cliente cliente, String newPass){
+        SimpleMailMessage sm = prepareNewPasswordEmail(cliente, newPass);
+        sendEmail(sm);
+    }
+
+    protected SimpleMailMessage prepareNewPasswordEmail(Cliente cliente, String newPass){
+        SimpleMailMessage sm = new SimpleMailMessage();
+        sm.setTo(cliente.getEmail());
+        sm.setFrom(sender);
+        sm.setSubject("Solicitacao nova Senha!");
+        sm.setSentDate(new Date(System.currentTimeMillis()));
+        sm.setText("Nova senha " + newPass);
         return sm;
     }
 }
--- src/main/java/br/com/everton/services/EmailService.java
+++ src/main/java/br/com/everton/services/EmailService.java
@@ -1,9 +1,11 @@
 package br.com.everton.services;
 
+import br.com.everton.domain.Cliente;
 import br.com.everton.domain.Pedido;
 import org.springframework.mail.SimpleMailMessage;
 
 public interface EmailService {
     void sendOrderConfirmationEmail(Pedido obj);
     void sendEmail(SimpleMailMessage msg);
+    void sendNewpasswordEmail(Cliente cliente, String newPass);
 }
